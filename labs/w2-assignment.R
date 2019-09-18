fe3258f748e39d7b6b20d439f74c1fd6f38f701a
Sumit Mukhija <mukhijas@lg12-26.scss.tcd.ie>
2019-09-18 10:31:36 +0100

    Week 2

---
+++ labs/w2-assignment.R
@@ -0,0 +1,17 @@
+library(rpart)
+library(partykit)
+Data <- read.csv("/users/pgrad/mukhijas/Desktop/DA-Labs/labs/data/DT-Credit.csv", header = TRUE, sep = ";")
+str(Data)
+attach(Data)
+DT_Model <- rpart(RESPONSE~., data=Data, control=rpart.control(minsplit=60, minbucket=30, maxdepth=4 ))
+plot(as.party((DT_Model)))
+print(DT_Model)
+
+Target = ifelse(RESPONSE == 1, 'Y', 'N')
+New_Data <- data.frame(Data, Target)
+str(New_Data)
+
+New_Data = Data[, -32]
+DT_Model_New <- rpart(Target~., data=New_Data, control=rpart.control(minsplit=60, minbucket=30, maxdepth=4 ))
+plot(as.party((DT_Model_New)))
+print(DT_Model_New)
