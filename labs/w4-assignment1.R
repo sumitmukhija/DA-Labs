e3b19f7653ceb24cd929669ad2d18b7b53dc81eb
Sumit Mukhija <mukhijas@lg12-35.scss.tcd.ie>
2019-10-02 10:48:53 +0100

    Fixes

--- labs/w4-assignment1.R
+++ labs/w4-assignment1.R
@@ -24,11 +24,13 @@ plot(as.party((DT_Model_XY_group_0)))
 
 df_with_X_group_0 = data.frame(df)
 df_with_X_group_0 = df_with_X_group_0[df_with_X_group_0$Group == 0,]
+df_with_X_group_0 = within(df_with_X_group_0, rm(Y1, Y2, Y3, Y4, Y5, Y6, Y7))
 DT_Model_X_group_0 <- rpart(Response~., data=df_with_X_group_0, control=rpart.control(minsplit=60, minbucket=30, maxdepth=4 ))
 plot(as.party((DT_Model_X_group_0)))
 
 df_with_Y_group_0 = data.frame(df)
 df_with_Y_group_0 = df_with_Y_group_0[df_with_Y_group_0$Group == 0,]
+df_with_Y_group_0 = within(df_with_Y_group_0, rm(X1, X2, X3, X4, X5, X6, X7))
 DT_Model_Y_group_0 <- rpart(Response~., data=df_with_Y_group_0, control=rpart.control(minsplit=60, minbucket=30, maxdepth=4 ))
 plot(as.party((DT_Model_Y_group_0)))
 
